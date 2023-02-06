5ee9f283a58e19c25b75e7a0a83ecbe8ab793cb8
Presley <persleytieman@outlook.com>
2023-02-06 15:23:26 -0500

    home button works

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ function Navigation() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Garden Planner</a>
+          <a class="navbar-brand" href="/">Garden Planner</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarColor03">
             <ul class="navbar-nav me-auto">
               <li class="nav-item">
-                <a class="nav-link active" href="#">Home
+                <a class="nav-link active" href="/">Home
                   <span class="visually-hidden">(current)</span>
                 </a>
               </li>
